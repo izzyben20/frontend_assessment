4b15fffc2f3f90915a0652fedae69c6885597a48
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-07-15 23:48:52 +0100

    added a helper function to convert repo stars and issues count

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -6,7 +6,6 @@ const Github = () => {
   const url =
     'https://api.github.com/search/repositories?q=created:>2021-08-13&sort=stars&order=desc';
   const { data, loading, error, fetchData } = useFetch(url);
-  console.log(data, loading, error);
 
   useEffect(() => {
     fetchData();
@@ -14,47 +13,59 @@ const Github = () => {
 
   if (loading) {
     return (
-      <section className='spinner'>
+      <section className="spinner">
         <SpinnerCircularFixed color="#01bf71" />
       </section>
     );
   }
 
+  if (data) {
+    console.log(data.items);
+  }
+
   if (error) {
     return (
-      <section className='spinner'>
+      <section className="spinner">
         <h4>{error}</h4>
       </section>
     );
   }
 
+  const convertNumber = (number) => {
+    return Math.abs(number) > 999
+      ? (Math.abs(number) / 1000).toFixed(1) + 'k'
+      : number;
+  };
+
   return (
     <section className="container">
-      <div className="user">
-        <div className="img_container">
-          <img src={data?.items[0].owner.avatar_url} alt="avatar" />
-        </div>
-        <div className="group">
-          <h2>{data?.items[0].name}</h2>
-          <p>{data?.items[0].description}</p>
-          <div className="flex-container">
-            <div className="border">
-              Stars: {data?.items[0].stargazers_count}
+      {data &&
+        data.items.map((item) => (
+          <div className="user" key={item.id}>
+            <div className="img_container">
+              <img src={item.owner.avatar_url} alt="avatar" />
             </div>
-            <div className="border">
-              Issues: {data?.items[0].open_issues_count}
+            <div className="group">
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <div className="flex-container">
+                <div className="border">
+                  Stars:{' '}
+                  {convertNumber(item.stargazers_count)}
+                </div>
+                <div className="border">Issues: {convertNumber(item.open_issues_count)}</div>
+                <p>
+                  Submitted{' '}
+                  {new Date(item.created_at).toLocaleString('en-us', {
+                    // month: 'short',
+                    day: '2-digit',
+                  })}{' '}
+                  days ago by {item.owner.login}
+                </p>
+              </div>
             </div>
-            <p>
-              Submitted{' '}
-              {new Date(data?.items[0].created_at).toLocaleString('en-us', {
-                // month: 'short',
-                day: '2-digit',
-              })}{' '}
-              days ago by {data?.items[0].owner.login}
-            </p>
           </div>
-        </div>
-      </div>
+        ))}
     </section>
   );
 };
