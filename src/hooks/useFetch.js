6ac90136b586663b2a13362b4265cbe7de3ec3fc
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-07-15 19:05:13 +0100

    used prettier to format code

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,24 +1,24 @@
 import { useState } from 'react';
 
 const useFetch = (url) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    
-    const fetchData = async () => {
-        setLoading(true);
-        try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setData(data);
-        setLoading(false);
-        } catch (error) {
-        setError(error);
-        setLoading(false);
-        }
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
     }
-    
-    return { data, loading, error, fetchData };
-    }
+  };
+
+  return { data, loading, error, fetchData };
+};
 
 export default useFetch;
