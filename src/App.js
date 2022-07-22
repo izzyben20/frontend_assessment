2bc66688fcb5603059b62f8eed4aae3e20a66b00
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-07-22 12:11:54 +0100

    added React Router for navigation with pages and created routes for the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-// import Github from './components/Github';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Github from './components/Github';
 import About from './pages/About';
+import ComingSoon from './pages/ComingSoon';
 
 function App() {
   return (
-    <div className="App">
-    <About />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ComingSoon />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/github" element={<Github />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
