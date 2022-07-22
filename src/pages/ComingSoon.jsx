fafa7b7e78b37504d06a2f5b9d0eccde73010852
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-07-22 12:07:41 +0100

    created the comming soon page which serve as the index page

---
+++ src/pages/ComingSoon.jsx
@@ -0,0 +1,16 @@
+import Main from "../components/Main"
+import Navbar from "../components/Navbar"
+
+
+const ComingSoon = () => {
+  return (
+    <section className='about-us coming-soon'>
+        <div className='about-container'>
+            <Navbar />
+        </div>
+        <Main />
+    </section>
+  )
+}
+
+export default ComingSoon
